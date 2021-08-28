936889eb77afd8a28b88e4222f96c4be5d6928d7
Daniil Gentili <daniil@daniil.it>
2021-08-28 11:40:59 +0200

    Fixes

--- src/Psalm/Internal/Codebase/Scanner.php
+++ src/Psalm/Internal/Codebase/Scanner.php
@@ -3,6 +3,7 @@ namespace Psalm\Internal\Codebase;
 
 use Psalm\Codebase;
 use Psalm\Config;
+use Psalm\Internal\Analyzer\IssueData;
 use Psalm\Internal\ErrorHandler;
 use Psalm\Internal\Provider\FileProvider;
 use Psalm\Internal\Provider\FileReferenceProvider;
