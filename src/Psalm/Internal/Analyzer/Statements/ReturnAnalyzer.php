4042979a886bf0954935048e69796c1915696cbd
Daniil Gentili <daniil@daniil.it>
2021-11-12 15:12:34 +0100

    Fixes

--- src/Psalm/Internal/Analyzer/Statements/ReturnAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/ReturnAnalyzer.php
@@ -62,14 +62,14 @@ class ReturnAnalyzer
             $file_storage = $file_storage_provider->get($statements_analyzer->getFilePath());
 
             try {
-                $var_comments = CommentAnalyzer::arrayToDocblocks(
+                $var_comments = []; /*CommentAnalyzer::arrayToDocblocks(
                     $doc_comment,
                     $parsed_docblock,
                     $statements_analyzer->getSource(),
                     $statements_analyzer->getAliases(),
                     $statements_analyzer->getTemplateTypeMap(),
                     $file_storage->type_aliases
-                );
+                );*/
             } catch (DocblockParseException $e) {
                 if (IssueBuffer::accepts(
                     new InvalidDocblock(
