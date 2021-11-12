aed4a90f77eb733fc9e2785cbdcc3fc49c04e34e
Daniil Gentili <daniil@daniil.it>
2021-11-12 14:13:42 +0100

    Disable @var parsing

--- src/Psalm/Internal/Analyzer/StatementsAnalyzer.php
+++ src/Psalm/Internal/Analyzer/StatementsAnalyzer.php
@@ -473,7 +473,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                 $var_comments = [];
 
                 try {
-                    $var_comments = CommentAnalyzer::arrayToDocblocks(
+                    /*$var_comments = CommentAnalyzer::arrayToDocblocks(
                         $docblock,
                         $statements_analyzer->parsed_docblock,
                         $statements_analyzer->getSource(),
@@ -481,6 +481,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                         $template_type_map,
                         $file_storage->type_aliases
                     );
+                    */
                 } catch (IncorrectDocblockException $e) {
                     IssueBuffer::maybeAdd(
                         new MissingDocblockType(
@@ -497,7 +498,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                     );
                 }
 
-                foreach ($var_comments as $var_comment) {
+                /*foreach ($var_comments as $var_comment) {
                     AssignmentAnalyzer::assignTypeFromVarDocblock(
                         $statements_analyzer,
                         $stmt,
@@ -511,7 +512,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                     ) {
                         $statements_analyzer->setFQCLN((string)$var_comment->type);
                     }
-                }
+                }*/
             }
         } else {
             $statements_analyzer->parsed_docblock = null;
