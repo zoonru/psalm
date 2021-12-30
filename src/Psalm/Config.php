32443e7c51c78202c9fb0b21b4fa45e2432320a1
Daniil Gentili <daniil@daniil.it>
2021-12-30 11:23:04 +0100

    PHP 8.2: seal all properties by default, add configuration key for lower versions

--- src/Psalm/Config.php
+++ src/Psalm/Config.php
@@ -347,6 +347,11 @@ class Config
     /**
      * @var bool
      */
+    public $seal_all_properties = false;
+
+    /**
+     * @var bool
+     */
     public $memoize_method_calls = false;
 
     /**
@@ -910,6 +915,7 @@ class Config
             'reportMixedIssues' => 'show_mixed_issues',
             'skipChecksOnUnresolvableIncludes' => 'skip_checks_on_unresolvable_includes',
             'sealAllMethods' => 'seal_all_methods',
+            'sealAllProperties' => 'seal_all_properties',
             'runTaintAnalysis' => 'run_taint_analysis',
             'usePhpStormMetaPath' => 'use_phpstorm_meta_path',
             'allowInternalNamedArgumentsCalls' => 'allow_internal_named_arg_calls',
