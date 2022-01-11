f86ebc7a4c2f3bf249a2eda54c118289550b3f6c
Daniil Gentili <daniil@daniil.it>
2022-01-11 10:40:36 +0100

    Squash fixes

--- src/Psalm/Internal/Analyzer/StatementsAnalyzer.php
+++ src/Psalm/Internal/Analyzer/StatementsAnalyzer.php
@@ -22,7 +22,6 @@ use Psalm\Internal\Analyzer\Statements\BreakAnalyzer;
 use Psalm\Internal\Analyzer\Statements\ContinueAnalyzer;
 use Psalm\Internal\Analyzer\Statements\EchoAnalyzer;
 use Psalm\Internal\Analyzer\Statements\Expression\Assignment\InstancePropertyAssignmentAnalyzer;
-use Psalm\Internal\Analyzer\Statements\Expression\AssignmentAnalyzer;
 use Psalm\Internal\Analyzer\Statements\Expression\Fetch\ClassConstFetchAnalyzer;
 use Psalm\Internal\Analyzer\Statements\Expression\Fetch\ConstFetchAnalyzer;
 use Psalm\Internal\Analyzer\Statements\Expression\Fetch\VariableFetchAnalyzer;
@@ -442,7 +441,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                 $var_comments = [];
 
                 try {
-                    $var_comments = CommentAnalyzer::arrayToDocblocks(
+                    /*$var_comments = CommentAnalyzer::arrayToDocblocks(
                         $docblock,
                         $statements_analyzer->parsed_docblock,
                         $statements_analyzer->getSource(),
@@ -450,6 +449,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                         $template_type_map,
                         $file_storage->type_aliases
                     );
+                    */
                 } catch (IncorrectDocblockException $e) {
                     IssueBuffer::maybeAdd(
                         new MissingDocblockType(
@@ -466,7 +466,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                     );
                 }
 
-                foreach ($var_comments as $var_comment) {
+                /*foreach ($var_comments as $var_comment) {
                     AssignmentAnalyzer::assignTypeFromVarDocblock(
                         $statements_analyzer,
                         $stmt,
@@ -480,7 +480,7 @@ class StatementsAnalyzer extends SourceAnalyzer
                     ) {
                         $statements_analyzer->setFQCLN((string)$var_comment->type);
                     }
-                }
+                }*/
             }
         } else {
             $statements_analyzer->parsed_docblock = null;
